784b0cf9bc39e3d2c75c5c2d430b1ea69e603d3c
BirukM14 <gbiruka14@gmail.com>
2025-02-24 09:57:31 +0300

    add Todo components and update App layout for task management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,22 @@ import { Box } from '@chakra-ui/react'
 import { useColorModeValue } from './components/ui/color-mode'
 import './App.css'
 import Navbar from './components/ui/Navbar'
+import TodoForm from './components/ui/TodoForm'                                 
+import TodoList from './components/ui/TodoList'
 
 function App() {
   return (
     // Full-screen box with dynamic background
     <Box 
       bg={useColorModeValue("white", "gray.900")} 
-      minH="100vh" // Ensures full height
-      w="100vw"    // Ensures full width
+      minH="90vh" // Ensures full height
+      w="50vw"    // Ensures full width
       display="flex"
       flexDirection="column"
     >
       <Navbar />
-      {/* Other components go here */}
+      <TodoForm/>
+      <TodoList/>
     </Box>
   );
 }
